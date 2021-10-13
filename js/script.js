05490ccf001a9316ba558b67b861f893820bdecd
SergeyNevinskiy <88575089+SergeyNevinskiy@users.noreply.github.com>
2021-10-13 20:31:52 +0300

    Hw3

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let admin;
+let name = "Джон";
+admin = name; 
+alert(admin);
+
+const ourPlanetName = "Земля";
+let currentUserName = "Владимир";
+
+let num1 = 10;
+let num2 = "10";
+
+let result = num1 + num2;
+
+console.log(result, typeof result);
